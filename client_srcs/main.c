b4b0af85eac176c221cf9db03faea92b1e43b100
Clement LACAILLE <clacaill@e2r6p9.clusters.42paris.fr>
2023-03-13 23:31:44 +0100

    One of start

--- client_srcs/main.c
+++ client_srcs/main.c
@@ -6,13 +6,59 @@
 /*   By: clacaill <clacaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 01:22:43 by clacaill          #+#    #+#             */
-/*   Updated: 2023/03/12 19:24:16 by clacaill         ###   ########.fr       */
+/*   Updated: 2023/03/13 22:22:14 by clacaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client_srcs.h"
 
-int main()
+void send_signal(int pid, char *message);
+
+int main(int argc, char *argv[])
 {
-	return (0);
+	if (argc != 3)
+	{
+		ft_printf( "Usage: ./client <server_pid> <message>\n");
+		return(-1);
+	}
+
+	int server_pid = ft_atoi(argv[1]);
+	char *message = argv[2];
+
+	send_signal(server_pid, message);
+	return 0;
+}
+
+void send_signal(int pid, char *message)
+{
+	unsigned char bits;
+	unsigned long int i;
+	unsigned long int j;
+
+	bits = 0;
+	i = 0;
+	j = 0;
+	while(message[i])
+	{
+		bits = (bits << 8) | message[i];
+		j = 0;
+		while(j < 8)
+		{
+			if ((bits & (1 << j)) != 0) //1
+				kill(pid, SIGUSR1);
+			else // 0
+				kill(pid, SIGUSR2);
+			usleep(800);
+			j++;
+		}
+		i++;
+	}
+	j = 0;
+	while (j < 8)
+	{
+		kill(pid, SIGUSR1); // end
+		usleep(800);
+		j++;
+	}
+	return;
 }
--- server_srcs/main.c
+++ server_srcs/main.c
@@ -6,18 +6,93 @@
 /*   By: clacaill <clacaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 01:22:33 by clacaill          #+#    #+#             */
-/*   Updated: 2023/03/12 20:25:22 by clacaill         ###   ########.fr       */
+/*   Updated: 2023/03/13 23:30:06 by clacaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server_srcs.h"
 
+int	ft_strlen_spe(unsigned char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s && *(s + i))
+		i++;
+	return (i);
+}
+
+unsigned char	*add_buff(unsigned char *buff, unsigned char count)
+{
+	int				len;
+	unsigned char	*temp;
+	int				i;
+
+	i = -1;
+	len = 0;
+	if(ft_strlen_spe(buff) != 0)
+		len = ft_strlen_spe(buff);
+	temp = malloc((len + 2) * sizeof(char));
+	if (!temp)
+		return(ft_free(&buff));
+	if (len == 0)
+	{
+		temp[0] = count;
+		temp[1] = '\0';
+	}
+	else
+	{
+		while(buff[++i])
+			temp[i] = buff[i];
+		ft_free(&buff);
+		temp[i] = count;
+		temp[++i] = '\0';
+	}
+	return(temp);
+}
+
+void action(int signum, siginfo_t *info, void *context __attribute__((unused)))
+{
+	static unsigned char bits = 0;
+	static int count = 0;
+	static unsigned char	*buff = NULL;
+	pid_t client_pid = info->si_pid;
+
+	if (client_pid && signum == SIGUSR1)
+		count = count | 1 << bits;
+	bits++;
+	if (bits == 8 && count != 255)
+	{
+		buff = add_buff(buff, count); // Enregistrer le caractère
+		count = 0;
+		bits = 0;
+	}
+	else if (bits == 8 && count == 255)
+	{
+		ft_printf("%s",buff);
+		ft_printf("\n\nEnd of the transmission.\n\n");
+		ft_free(&buff);
+		bits = 0;
+		count = 0;
+		buff = NULL;
+	}
+}
+
 int main()
 {
-    signal(SIGINT, signal_handler);
+	struct sigaction	srv_action;
+	sigset_t			set;
 
-    printf("Waiting for signal...\n");
-    pause();
-    printf("Resuming execution.\n");
-	return (0);
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigaddset(&set, SIGUSR2);
+	srv_action.sa_mask = set;
+	srv_action.sa_sigaction = action;
+	srv_action.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &srv_action, 0);
+	sigaction(SIGUSR2, &srv_action, 0);
+	printf("Server PID: %d\n", getpid());
+	while (1)
+		pause();
+	return 0;
 }
